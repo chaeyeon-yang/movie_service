6afaba11b38c444f651313470b4f30c7c387636b
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2022-01-17 15:26:15 +0900

    prepare routing

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -8,3 +8,10 @@ function App() {
 export default App;
 
 //App.js 는 router를 render 한다.
+
+//home 라우트(페이지)는 모든 영화를 보여주고
+//Movie 라우트는 영화 하나만 보여준다.
+//라우트 별로 생각해야함
+//home라우트는 기본적으로 App 컴포넌트 전체를 가지고 있게 만든다.
+
+//App 에 있는 것을 모두 Home 라우트로 옮겼으니 App.js는 라우터를 렌더 한다.
